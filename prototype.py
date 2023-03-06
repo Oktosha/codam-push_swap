0354caf1b6fd9578f40619bb10a5f8a60c2d4bfe
Daria Kolodzey <kolodaria@gmail.com>
2023-03-06 08:48:58 +0100

    make mergesort calc take NlogN

--- prototype.py
+++ prototype.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 a = []
 for arg in sys.argv[1:]:
@@ -6,11 +7,14 @@ for arg in sys.argv[1:]:
 #print(a)
 
 if len(a) == 0:
-    a = [6, 5, 4, 3, 2, 1]
+    a = [x for x in range(1000000)]
+    a.reverse()
 
+print(f"check; a[0] = {a[0]}, a[-1] = {a[-1]}")
+a = deque(a)
 element_count = len(a)
 sorted_length = 1
-b = []
+b = deque()
 ans = []
 while sorted_length < element_count:
     pos = 0
@@ -22,21 +26,24 @@ while sorted_length < element_count:
             for i in range(first_half_length):
                 ans.append("pb")
                 ans.append("rb")
-                b.append(a.pop(0))
+                b.append(a.popleft())
             pos_a = 0
             while pos_a < second_half_length or len(b) > 0:
                 if len(b) == 0 or (pos_a < second_half_length and a[0] < b[0]):
                     ans.append("ra")
-                    a = a[1:] + [a[0]]
+                    a.append(a.popleft())
                     pos_a += 1
                 else:
                     ans.append("pa")
                     ans.append("ra")
-                    a.append(b.pop(0))
+                    a.append(b.popleft())
         else:
             ans.extend(["ra"] * current_segment_length)
-            a = a[current_segment_length:] + a[:current_segment_length]
+            # a = a[current_segment_length:] + a[:current_segment_length]
+            a.rotate(-current_segment_length)
         pos += current_segment_length
     sorted_length *= 2
 
-print("\n".join(ans))
+# print(a)
+# print("\n".join(ans))
+print(len(ans))
